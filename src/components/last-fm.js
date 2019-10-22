a46786d807b237a0c1ababd9f4f57eca205aef25
Larry Hudson <larryhudson@gmail.com>
2019-10-22 21:40:37 +1100

    first last.fm stuff

---
+++ src/components/last-fm.js
@@ -0,0 +1,87 @@
+import React from "react"
+import axios from "axios"
+
+import "./last-fm.scss"
+
+export default () => {
+  const [tracks, setTracks] = React.useState([])
+  const [apiResponse, setApiResponse] = React.useState(null)
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState(null)
+
+  React.useEffect(() => {
+    setLoading(true)
+    axios
+      .get("http://ws.audioscrobbler.com/2.0/", {
+        params: {
+          method: "user.getrecenttracks",
+          user: "harryludson",
+          api_key: "43c1cbeecc3f5b3e29fa28fb740255bc",
+          format: "json",
+          limit: "8",
+        },
+      })
+      .then(response => {
+        setApiResponse(response.data)
+        setTracks(
+          response.data.recenttracks.track.map(track => ({
+            artist: track.artist["#text"],
+            album: track.album["#text"],
+            name: track.name,
+            image: track.image[3]["#text"],
+            url: track.url,
+          }))
+        )
+        setLoading(false)
+      })
+      .catch(error => {
+        setLoading(false)
+        setError(error.message)
+      })
+    setLoading(false)
+  }, [])
+  return (
+    <div>
+      <h2>What is Larry listening to?</h2>
+      <p>
+        This is getting information from my{" "}
+        <a href="https://last.fm/user/harryludson">Last.fm page</a>.
+      </p>
+      {loading && <Loading />}
+      {error && <Error error={error} />}
+      {tracks.length > 0 && <Tracks tracks={tracks} />}
+      {apiResponse && <pre>{JSON.stringify(apiResponse, null, 4)}</pre>}
+    </div>
+  )
+}
+
+const Loading = () => <div>Loading...</div>
+
+const Error = ({ error }) => <div>Error! {error.message}</div>
+
+const Tracks = ({ tracks }) => {
+  return (
+    <ol className="tracks">
+      {tracks.map(track => (
+        <Track {...track} />
+      ))}
+    </ol>
+  )
+}
+
+const Track = ({ url, name, artist, album, image }) => {
+  return (
+    <li>
+      <div className="img-container">
+        <img src={image} alt="" />
+      </div>
+      <div className="text">
+        <a className="song-link" href={url}>
+          {name}
+        </a>
+        <span className="artist-name">{artist}</span>
+        <span className="album">{album}</span>
+      </div>
+    </li>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 import React from "react"
+import LastFm from "../components/last-fm"
 
-export default () => <div>Hello world!</div>
+import "./global.scss"
+
+export default () => (
+  <div>
+    <h1>My fun music site</h1>
+    <p>
+      This is going to be a playground for me to play around with different ways
+      of organising music information:
+    </p>
+    <ul>
+      <li>my bootlegs</li>
+      <li>information from my Last.fm profile</li>
+    </ul>
+    <p>Narcissistic but fun!</p>
+    <LastFm />
+  </div>
+)
